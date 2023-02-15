741665d6c71720de77546106fb30f7614c5d3d05
Hanqi Xiao <theinscrutableengineer@gmail.com>
2023-02-15 17:09:39 -0500

    Updates to project description

--- search_zotero_integration.py
+++ search_zotero_integration.py
@@ -10,6 +10,13 @@ Correction: Boolean Search appears to work, but longer queries are cut off and t
 usual precedence, although && is still higher in precedence than OR, and presumably | is higher in precedence than AND (it so far seems this way).
 
 Limit search to 2003-2023.
+
+Note for the Zotero API:
+You can quickly add items to your library if you already know their ISBN, DOI, PubMed ID, or arXiv ID. 
+
+For searching the zotero data base, a potential solution is found for the constriction
+only problem that was pointed out by Riley:
+tag search. See the Search Syntax for details. More than one tag may be passed by passing a list of strings – These are treated as AND search terms, meaning only items which include all of the specified tags are returned. You can search for items matching any tag in a list by using OR: "tag1 OR tag2", and all items which exclude a tag: "-tag".
 """
 import pprint
 from time import sleep
@@ -32,7 +39,7 @@ for i in range(1):
     sleep(1.23562)
 
 with open("publications.txt", "w") as f:
-    f.write(pprint.format(publications[0]))
+    f.write(pprint.pformat(publications[0]))
     pass
 # scholarly.pprint(next(responses))
 # scholarly.pprint(next(responses))
